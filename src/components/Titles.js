233b90a1d2f91fd62aad6853bf15a54ce7c2fcd3
Elie-Paul <eliepaulmoubotouto@gmail.com>
2020-03-31 12:32:52 +0000

    first commit

---
+++ src/components/Titles.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Titles = () => (
+	<div>
+		<h1 className="title-container__title">Recherche la météo</h1>
+		<h3 className="title-container__subtitle">Découvrez la température, les conditions et plus...</h3>
+	</div>
+);
+
+export default Titles;
